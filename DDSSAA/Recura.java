fd1021e4bc6c53049a07b7ac5f15185ca1537a31
SubashSK777 <subashsk11831@gmail.com>
2025-05-25 05:04:38 -0700

    2025-05-25T12:04:38.381Z:

--- DDSSAA/Recura.java
+++ DDSSAA/Recura.java
@@ -4,11 +4,13 @@ import java.util.Scanner;
 public class Recura {
 
     public static int count(int i, int n) {
-
+        if (i > n) {
+            return;
+        }
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
         System.out.println(count(1, n));
-        int n = sc.
     }
 }
