39f1dcc7bf8ee6910f61a3ef9a5ece6ab9a22b8a
SubashSK777 <subashsk11831@gmail.com>
2025-05-25 04:29:14 -0700

    2025-05-25T11:29:14.032Z:

--- DDSSAA/Recura.java
+++ DDSSAA/Recura.java
@@ -1,7 +1,11 @@
 public class Recura {
 
     public static int recusive1 (int n){
-        if (n == 0)
+        if (n == 0){
+            return 1;
+        } else {
+            return recusive1(n - 1) + n;
+        }
     }
     public static void main(String[] args) {
         
