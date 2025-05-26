6231c15254476563bc55540fd0bbc075500fa620
SubashSK777 <subashsk11831@gmail.com>
2025-05-25 22:46:00 -0700

    2025-05-26T05:46:00.720Z:

--- DDSSAA/Recura.java
+++ DDSSAA/Recura.java
@@ -1,5 +1,4 @@
 
-import java.util.Scanner;
 
 public class Recura {
 
@@ -8,9 +7,21 @@ public class Recura {
         System.out.print(i + " ");
         count(i + 1, n);
     }
+
+    public static void rev_count(int i) {
+        if (i == 0) return;
+        System.out.print(i + " ");
+        rev_count(i - 1);
+    }
+
+    public static int sum_n(int n) {
+        if (n == 0) return 0;
+        return n + sum_n(n - 1);
+    }
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        count(1, n);
+        // count(1, 10);
+        // rev_count(10);
+        System.out.print(sum_n(10));
     }
 }
--- DDSSAA/Recurva.py
+++ DDSSAA/Recurva.py
@@ -5,9 +5,20 @@ def count(i, n):
         print(i, end=" ")
         return count(i + 1, n)
     
-def rev_count(i, n):
+def rev_count(i):
     if i == 0:
         return
     else:
-        print
-count(1, 10)
+        print(i, end=" ")
+        return rev_count(i - 1)
+    
+def sum_n(n):
+    if n == 0: return 0
+    return sum_n(n - 1) + n
+
+def factorial(n):
+    if n
+
+# count(1, 10) 
+# rev_count(10)
+print(sum_n(10))
