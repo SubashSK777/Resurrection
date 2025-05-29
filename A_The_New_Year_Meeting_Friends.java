98ff9a34d923abeefb5c3ccc70796e6c0f12fa03
SubashSK777 <subashsk11831@gmail.com>
2025-05-29 06:35:59 -0700

    2025-05-29T13:35:59.339Z:

--- A_The_New_Year_Meeting_Friends.java
+++ A_The_New_Year_Meeting_Friends.java
@@ -3,7 +3,11 @@ import java.util.Scanner;
 
 public class A_The_New_Year_Meeting_Friends {
     public static void main(String[] args) {
-        Scanner sc = Scanner(System.in);
-        
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int z = sc.nextInt();
+
+        int
     }
 }
