4f3a20a67e19be55d6084f3541dafd25ab98613f
SubashSK777 <subashsk11831@gmail.com>
2025-05-29 02:44:22 -0700

    2025-05-29T09:44:22.179Z:

--- A_ChewbaCca_and_Number.java
+++ A_ChewbaCca_and_Number.java
@@ -7,8 +7,8 @@ public class A_ChewbaCca_and_Number {
         String s = sc.nextLine();
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < s.length(); i++){
-            char d = s.charAt(i) - '0';
-            
+            int d = s.charAt(i) - '0';
+
         }
 
     }
