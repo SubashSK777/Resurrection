b20a829185303e87a041d93e4d1e683ca1508e9f
SubashSK777 <subashsk11831@gmail.com>
2025-05-28 18:43:20 -0700

    2025-05-29T01:43:20.711Z:

--- A_Bit.java
+++ A_Bit.java
@@ -6,10 +6,13 @@ class  A_Bit {
         int A = 0;
 
         Scanner sc = new Scanner(System.in);
+        int n =
         for (int i = 0; i < 2; i++) {
             String s = sc.nextLine();
             if (s.contains("++")) A++;
             else A--;
         }
+
+        System.out.println(A);
     }
 }
