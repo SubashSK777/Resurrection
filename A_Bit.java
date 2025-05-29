381d90a68661f36926a3fc56d726b7b9196a2a7e
SubashSK777 <subashsk11831@gmail.com>
2025-05-28 18:40:12 -0700

    2025-05-29T01:40:12.229Z:

--- A_Bit.java
+++ A_Bit.java
@@ -2,6 +2,8 @@ class  A_Bit {
     public static void main(String[] args) {
         int A = 0;
 
-        for (int i = 0; )
+        for (int i = 0; i < 2; i++) {
+            
+        }
     }
 }
