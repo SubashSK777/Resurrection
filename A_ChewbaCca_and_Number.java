8ebbf76c8bc795aa74a56f5090a3276154d4c5b1
SubashSK777 <subashsk11831@gmail.com>
2025-05-29 03:50:39 -0700

    2025-05-29T10:50:39.169Z:

--- A_ChewbaCca_and_Number.java
+++ A_ChewbaCca_and_Number.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class A_Chewbacca_and_Number {
+public class A_ChewbacCa_and_Number {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
