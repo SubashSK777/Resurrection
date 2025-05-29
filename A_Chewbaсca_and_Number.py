f8ffe1a2964f0d1e0ede1e85287aa6d74a973aa4
SubashSK777 <subashsk11831@gmail.com>
2025-05-29 03:58:56 -0700

    2025-05-29T10:58:56.774Z:

--- A_Chewbaсca_and_Number.py
+++ A_Chewbaсca_and_Number.py
@@ -4,4 +4,7 @@ for i in range(len(s)):
     d = int(i)
     f = 9 - d
     
-    if (i == 0 and f == 0)
+    if (i == 0 and f == 0): arr.append(d)
+    else: arr.append(min(d, f))
+    
+print("".join(arr))
