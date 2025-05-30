8cda095d048b3bae89978151e06fc681f18c9817
SubashSK777 <subashsk11831@gmail.com>
2025-05-30 04:31:59 -0700

    2025-05-30T11:31:59.637Z:

--- A_Anton_and_Polyhedrons.java
+++ A_Anton_and_Polyhedrons.java
@@ -1,5 +1,7 @@
 public class A_Anton_and_Polyhedrons {
     public static void main(String[] args) {
-        
+        int r = 0;
+        Scanner sc = new Scanner(System.in);
+        int n =
     }
 }
