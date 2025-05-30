8ae7ca1b2809c281fd98ef743bf4f1bc1f8a2c25
SubashSK777 <subashsk11831@gmail.com>
2025-05-30 06:48:29 -0700

    2025-05-30T13:48:29.422Z:

--- Summer.java
+++ Summer.java
@@ -4,6 +4,10 @@ import java.io.*;
 public class Summer {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new)
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        
+
+        bw.flush();
     }
 }
