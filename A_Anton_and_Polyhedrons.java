c65fc2127d6651ea5ef3c23e9e0ff11435b9f261
SubashSK777 <subashsk11831@gmail.com>
2025-05-30 04:33:04 -0700

    2025-05-30T11:33:04.506Z:

--- A_Anton_and_Polyhedrons.java
+++ A_Anton_and_Polyhedrons.java
@@ -1,7 +1,13 @@
+
+import java.util.Scanner;
+
 public class A_Anton_and_Polyhedrons {
     public static void main(String[] args) {
         int r = 0;
         Scanner sc = new Scanner(System.in);
-        int n =
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            
+        }
     }
 }
